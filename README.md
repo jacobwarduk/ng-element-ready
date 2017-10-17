e4f4d3ae3342293ba3c1b2e9a3ad871b6037251b
Jacob Ward <jacob@candyboxmedia.com>
2017-10-17 07:55:37 +0100

    Finally publishing this directive

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng-element-ready
+
+Detect when an element is truly _ready_, e.g. it has finished being populated with fresh data.
---
+++ elementReady.js
@@ -0,0 +1,25 @@
+(angular => {
+	'use strict';
+
+	const elementReady = ($timeout, $rootScope) => {
+		return {
+			restrict: 'A',
+			link(scope, element, attrs) {
+				$timeout(() => {
+					element.ready(() => {
+						scope.$apply(() => {
+							$rootScope.$broadcast(`${attrs.elementReady}:ready`);
+						});
+					});
+				});
+			}
+		};
+	};
+
+	elementReady.$inject = ['$timeout', '$rootScope'];
+
+	angular
+		.module('ElementReady', [])
+		.directive('elementReady', elementReady)
+	;
+})();
